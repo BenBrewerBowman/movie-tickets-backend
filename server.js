041d763fb6ae41b366cd8c30c55c7f70155754ca
BenDaG <benbrewerbowman@gmail.com>
2017-10-08 22:08:05 -0600

    Server connected to mongoDB and router

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+
 import express from 'express';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import router from './router';
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/movies');
 
 // Initialize http server
 const app = express();
 
-// Handle / route
-app.get('/', (req, res) =>
-  res.send('Hello World!')
-)
+// Logger that outputs all requests into the console
+app.use(morgan('combined'));
+// Use v1 as prefix for all API endpoints
+app.use('/v1', router);
 
 // Launch the server on port 3000
 const server = app.listen(3000, () => {
